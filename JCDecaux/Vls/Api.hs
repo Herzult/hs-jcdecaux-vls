d459c5d27c58f1921911f14b9e3a8de0f7100611
Antoine Hérault <antoine.herault@gmail.com>
2013-08-24 17:49:23 +0200

    Add exception handling

--- JCDecaux/Vls/Api.hs
+++ JCDecaux/Vls/Api.hs
@@ -14,16 +14,20 @@ import JCDecaux.Vls.Types
 
 import Network.HTTP.Conduit (simpleHttp)
 
-import Control.Monad
 import Control.Monad.IO.Class
 
 import Data.Text (Text, pack, unpack, intercalate)
 import Data.String (IsString, fromString)
+import qualified Data.ByteString.Lazy.Char8 as LBS
 
 import Data.Aeson (FromJSON, eitherDecode)
 import Data.Monoid ((<>))
 
-type Error = String
+import qualified Control.Exception as E
+
+data Error = HttpConnectionError E.SomeException
+           | JsonError   Text
+           deriving (Show)
 
 type Path = [Text]
 
@@ -37,27 +41,38 @@ instance IsString ApiKey where
 type Result a = Either Error a
 
 contracts :: MonadIO m => ApiKey -> m (Result [Contract])
-contracts key = get key ["contracts"] []
+contracts key = liftIO $ get key ["contracts"] []
 
 stations :: MonadIO m => ApiKey -> m (Result [Station])
-stations key = get key ["stations"] []
+stations key = liftIO $ get key ["stations"] []
 
 contractStations :: MonadIO m
                  => ApiKey
                  -> Text -- ^ contract name
                  -> m (Result [Station])
-contractStations key cn = get key ["stations"] [("contract", cn)]
+contractStations key cn = liftIO $ get key ["stations"] [("contract", cn)]
 
 contractStation :: MonadIO m
                 => ApiKey
                 -> Text -- ^ contract name
                 -> Int  -- ^ station number
                 -> m (Result Station)
-contractStation key cn sn = get key ["stations", pack (show sn)] [("contract", cn)]
+contractStation key cn sn = liftIO $ get key ["stations", pack (show sn)] [("contract", cn)]
 
-get :: (MonadIO m, FromJSON a) => ApiKey -> Path -> QueryString -> m (Result a)
-get (ApiKey k) p q =
-    liftM eitherDecode (simpleHttp $ unpack url)
+get :: (FromJSON a) => ApiKey -> Path -> QueryString -> IO (Result a)
+get (ApiKey k) p q = do
+    (simpleHttp url >>= return . parse)
+        `E.catches` onException
+
   where
-    url   = "https://api.jcdecaux.com/vls/v1/" <> intercalate "/" p <> "?" <> query
+    onException = [ E.Handler (\e -> E.throw (e :: E.AsyncException))
+                  , E.Handler (\e -> return . Left $ HttpConnectionError (e :: E.SomeException))
+                  ]
+
+    parse b =
+        case eitherDecode b of
+            Left e  -> Left $ JsonError $ pack e <> " in JSON response: " <> pack (LBS.unpack b)
+            Right d -> Right d
+
+    url   = unpack $ "https://api.jcdecaux.com/vls/v1/" <> intercalate "/" p <> "?" <> query
     query = intercalate "&" $ map (\(n, v) -> n <> "=" <> v) (("apiKey", k):q)
